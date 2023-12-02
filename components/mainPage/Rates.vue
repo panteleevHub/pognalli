bc4d2bb923c8288ca8b33ecf0ea42c6a0df787af
Victor Panteleev <victorPanteleev99@gmail.com>
2023-12-02 14:44:41 +0300

    stylized rates block for tablet

--- components/mainPage/Rates.vue
+++ components/mainPage/Rates.vue
@@ -12,7 +12,10 @@
           <span>{{ price === 0 ? 'Бесплатно': `${price} ${currency}./мес`}}</span>
         </li>
       </ul>
-      <NuxtLink class="rates__link" to="/">Показать тарифы для бизнеса</NuxtLink>
+      <NuxtLink class="rates__link" to="/">
+        <span class="rates__link-icon"></span>
+        Показать тарифы для бизнеса
+      </NuxtLink>
     </div>
   </section>
 </template>
@@ -43,20 +46,39 @@ const rates = [
 <style lang="scss" scoped>
 .rates {
   background: url('@/assets/img/icon-rates.png') calc(100% - 25px) 38px no-repeat;
+
+  @media (min-width: $tablet-width) {
+    background-position: calc(100% - 85px) calc(100% - 45px);
+  }
 }
 
 .rates__container {
   padding: 38px 25px 38px 50px;
+
+  @media (min-width: $tablet-width) {
+    padding: 74px 40px 64px;
+  }
 }
 
 .rates__caption {
   min-height: 100px;
   padding: 16px 130px 12px 0;
+
+  @media (min-width: $tablet-width) {
+    min-height: auto;
+    padding: 0;
+    margin-bottom: 40px;
+    text-align: center;
+  }
 }
 
 .rates__list {
   @include reset-list;
   padding: 30px 0;
+
+  @media (min-width: $tablet-width) {
+    padding-right: 35px;
+  }
 }
 
 .rates__item {
@@ -67,6 +89,14 @@ const rates = [
   font-weight: 300;
   padding-top: 4px;
   padding-bottom: 15px;
+
+  @media (min-width: $tablet-width) {
+    @include flex-base;
+    flex-direction: row;
+    font-size: 28px;
+    line-height: 28px;
+    padding: 0;
+  }
 }
 
 .rates__item::before {
@@ -74,9 +104,14 @@ const rates = [
   position: absolute;
   top: 0;
   left: 0;
-  width: 82%;
+  right: 44px;
   height: 1px;
   background-color: $light-grey;
+
+  @media (min-width: $tablet-width) {
+    left: 92px;
+    right: 0;
+  }
 }
 
 .rates__item:last-child::after {
@@ -84,13 +119,23 @@ const rates = [
   position: absolute;
   bottom: 0;
   left: 0;
-  width: 82%;
+  right: 44px;
   height: 1px;
   background-color: $light-grey;
+
+  @media (min-width: $tablet-width) {
+    left: 92px;
+    right: 0;
+  }
 }
 
 .rates__name {
   @include flex-base;
+  column-gap: 22px;
+
+  @media (min-width: $tablet-width) {
+    flex-direction: row-reverse;
+  }
 }
 
 .rates__count {
@@ -102,6 +147,13 @@ const rates = [
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media (min-width: $tablet-width) {
+    width: 68px;
+    height: 68px;
+    font-size: 28px;
+    line-height: 28px;
+  }
 }
 
 .rates__item:nth-child(3n+1) .rates__count {
@@ -121,5 +173,26 @@ const rates = [
   line-height: 16px;
   font-weight: 700;
   color: $blue;
+
+  @media (min-width: $tablet-width) {
+    display: flex;
+    align-items: center;
+    column-gap: 20px;
+    max-width: 500px;
+    font-size: 20px;
+    line-height: 20px;
+  }
+}
+
+.rates__link-icon {
+  display: none;
+
+  @media (min-width: $tablet-width) {
+    display: block;
+    width: 68px;
+    height: 68px;
+    background: $light-grey url('@/assets/img/icon-businessman.svg') center no-repeat;
+    border-radius: 50%;
+  }
 }
 </style>
